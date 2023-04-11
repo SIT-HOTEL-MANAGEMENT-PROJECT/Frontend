8df8f18cba89c814daec765b7c9e428a5627e563
Prittha Dutta <pritthadatta@gmail.com>
2023-04-12 01:18:33 +0530

    Fixed Minor Bug in Home Page

--- src/components/Home3.jsx
+++ src/components/Home3.jsx
@@ -63,7 +63,7 @@ const Home3 = ({ resetAppData }) => {
           <div
             className="offcanvas offcanvas-start"
             data-bs-scroll="true"
-            tabindex="-1"
+            tabIndex="-1"
             id="offcanvasWithBothOptions"
             aria-labelledby="offcanvasWithBothOptionsLabel"
           >
