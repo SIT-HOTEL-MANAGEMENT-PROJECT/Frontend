c413ac716171435dceecc5196dfb5b2476690bbc
Prasun Roy <prasunroy212@gmail.com>
2023-04-12 06:00:04 +0530

    Refactored Team Page

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -3,15 +3,10 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import shreya from "../img/shreya.jpeg";
-import alik from "../img/alik.jpeg";
-import srishti from "../img/srishti.jpeg";
 import soumwadeep from "../img/soumwadeep.png";
 import prittha from "../img/prittha.jpeg";
-import sayantani from "../img/sayantani.jpeg";
 import prasun from "../img/prasun.jpeg";
 import sagnik from "../img/sagnik.jpeg";
-import subham from "../img/subham.jpeg";
-import arnab from "../img/arnab.png";
 import diya from "../img/diya.jpeg";
 const Team = () => {
   return (
@@ -77,11 +72,11 @@ const Team = () => {
           <div className="row">
             <div className="col-sm">
               <div className="card teamcard">
-                <img src={alik} className="card-img-top" alt="..." />
+                <img src={prittha} className="card-img-top" alt="..." />
                 <div className="card-body">
                   <center>
-                    <p className="card-text">Alik Sarkar</p>
-                    <a href="https://www.linkedin.com/in/alik-sarkar-901346225">
+                    <p className="card-text">Prittha Dutta</p>
+                    <a href="https://www.linkedin.com/in/prittha-datta-2679ba1b7">
                       <i
                         class="fa fa-linkedin-square"
                         aria-hidden="true"
@@ -89,55 +84,15 @@ const Team = () => {
                       ></i>
                     </a>
                     &nbsp;&nbsp;
-                    <a
-                      href="https://twitter.com/AlikSarkar16?t=wO12PN_jFAR1SSt
-E0tQlCg&s=09"
-                    >
+                    {/* <a href="#">
                       <i
-                        class="fa fa-twitter-square"
+                        class="fa fa-github-square"
                         aria-hidden="true"
                         id="socialiconfix"
                       ></i>
                     </a>
-                    &nbsp;&nbsp;
-                    <a
-                      href="https://instagram.com/alik_sarkar_18?igshid=ZDdkNTZ
-iNTM="
-                    >
-                      <i
-                        class="fa fa-instagram"
-                        aria-hidden="true"
-                        id="socialiconfix"
-                      ></i>
-                    </a>
-                    &nbsp;&nbsp;
-                  </center>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm">
-              <div className="card teamcard">
-                <img src={srishti} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <center>
-                    <p className="card-text">Srishti Majumder</p>
-                    <a href="https://www.linkedin.com/in/srishti-majumder-6560bb200/">
-                      <i
-                        class="fa fa-linkedin-square"
-                        aria-hidden="true"
-                        id="socialiconfix"
-                      ></i>
-                    </a>
-                    &nbsp;&nbsp;
-                    <a href="https://twitter.com/MajumderSrishti">
-                      <i
-                        class="fa fa-twitter-square"
-                        aria-hidden="true"
-                        id="socialiconfix"
-                      ></i>
-                    </a>
-                    &nbsp;&nbsp;
-                    <a href="https://www.instagram.com/srishti.majumder.7074/">
+                    &nbsp;&nbsp; */}
+                    <a href="https://instagram.com/the.pritthadatta?utm_medium=copy_link">
                       <i
                         class="fa fa-instagram"
                         aria-hidden="true"
@@ -183,77 +138,43 @@ iNTM="
                 </div>
               </div>
             </div>
-            <div className="col-sm">
-              <div className="card teamcard">
-                <img src={prittha} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <center>
-                    <p className="card-text">Prittha Dutta</p>
-                    <a href="https://www.linkedin.com/in/prittha-datta-2679ba1b7">
-                      <i
-                        class="fa fa-linkedin-square"
-                        aria-hidden="true"
-                        id="socialiconfix"
-                      ></i>
-                    </a>
-                    &nbsp;&nbsp;
-                    {/* <a href="#">
-                      <i
-                        class="fa fa-github-square"
-                        aria-hidden="true"
-                        id="socialiconfix"
-                      ></i>
-                    </a>
-                    &nbsp;&nbsp; */}
-                    <a href="https://instagram.com/the.pritthadatta?utm_medium=copy_link">
-                      <i
-                        class="fa fa-instagram"
-                        aria-hidden="true"
-                        id="socialiconfix"
-                      ></i>
-                    </a>
-                    &nbsp;&nbsp;
-                  </center>
-                </div>
-              </div>
-            </div>
           </div>
           <br />
           <h6 className="text-primary">Back-End Team :</h6>
           <div className="row">
             <div className="col-sm">
-              <div className="card teamcard">
-                <img src={sayantani} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <center>
-                    <p className="card-text">Sayantani Sarkar</p>
-                    <a href="https://www.linkedin.com/in/sayantani-sarkar- 026199200/">
-                      <i
-                        class="fa fa-linkedin-square"
-                        aria-hidden="true"
-                        id="socialiconfix"
-                      ></i>
-                    </a>
-                    &nbsp;&nbsp;
-                    <a href="https://twitter.com/Sayanta99188174">
-                      <i
-                        class="fa fa-twitter-square"
-                        aria-hidden="true"
-                        id="socialiconfix"
-                      ></i>
-                    </a>
-                    &nbsp;&nbsp;
-                    <a href="https://www.instagram.com/____aishaaa________/">
-                      <i
-                        class="fa fa-instagram"
-                        aria-hidden="true"
-                        id="socialiconfix"
-                      ></i>
-                    </a>
-                    &nbsp;&nbsp;
-                  </center>
+                <div className="card teamcard">
+                  <img src={diya} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <center>
+                      <p className="card-text">Diya Sarkar</p>
+                      <a href="#">
+                        <i
+                          class="fa fa-linkedin-square"
+                          aria-hidden="true"
+                          id="socialiconfix"
+                        ></i>
+                      </a>
+                      {/* // &nbsp;&nbsp;
+                     // <a href="#">
+                       // <i
+                         // class="fa fa-twitter-square"
+                          //aria-hidden="true"
+                          //id="socialiconfix"
+                        //></i>
+                      //</a>
+                     // &nbsp;&nbsp; */}
+                      <a href="#">
+                        <i
+                          class="fa fa-instagram"
+                          aria-hidden="true"
+                          id="socialiconfix"
+                        ></i>
+                      </a>
+                      &nbsp;&nbsp;
+                    </center>
+                  </div>
                 </div>
-              </div>
             </div>
             <div className="col-sm">
               <div className="card teamcard">
@@ -323,113 +244,7 @@ iNTM="
                 </div>
               </div>
             </div>
-            <div className="col-sm">
-              <div className="card teamcard">
-                <img src={subham} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <center>
-                    <p className="card-text">Subham</p>
-                    <a href="http://www.linkedin.com/in/shubham-divyanshu-a5a3a2258">
-                      <i
-                        class="fa fa-linkedin-square"
-                        aria-hidden="true"
-                        id="socialiconfix"
-                      ></i>
-                    </a>
-                    &nbsp;&nbsp;
-                    <a href="#">
-                      <i
-                        class="fa fa-twitter-square"
-                        aria-hidden="true"
-                        id="socialiconfix"
-                      ></i>
-                    </a>
-                    &nbsp;&nbsp;
-                    <a href="https://www.instagram.com/shubhamdevanshu/?igshid=YmMyMTA2M2Y%3D">
-                      <i
-                        class="fa fa-instagram"
-                        aria-hidden="true"
-                        id="socialiconfix"
-                      ></i>
-                    </a>
-                    &nbsp;&nbsp;
-                  </center>
-                </div>
-              </div>
-            </div>
           </div>
-          <center>
-            <div className="row">
-              <div className="col-sm">
-                <div className="card teamcard">
-                  <img src={arnab} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <center>
-                      <p className="card-text">Arnab Saha</p>
-                      <a href="#">
-                        <i
-                          class="fa fa-linkedin-square"
-                          aria-hidden="true"
-                          id="socialiconfix"
-                        ></i>
-                      </a>
-                      &nbsp;&nbsp;
-                      <a href="#">
-                        <i
-                          class="fa fa-twitter-square"
-                          aria-hidden="true"
-                          id="socialiconfix"
-                        ></i>
-                      </a>
-                      &nbsp;&nbsp;
-                      <a href="#">
-                        <i
-                          class="fa fa-instagram"
-                          aria-hidden="true"
-                          id="socialiconfix"
-                        ></i>
-                      </a>
-                      &nbsp;&nbsp;
-                    </center>
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm">
-                <div className="card teamcard">
-                  <img src={diya} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <center>
-                      <p className="card-text">Diya Sarkar</p>
-                      <a href="#">
-                        <i
-                          class="fa fa-linkedin-square"
-                          aria-hidden="true"
-                          id="socialiconfix"
-                        ></i>
-                      </a>
-                      {/* // &nbsp;&nbsp;
-                     // <a href="#">
-                       // <i
-                         // class="fa fa-twitter-square"
-                          //aria-hidden="true"
-                          //id="socialiconfix"
-                        //></i>
-                      //</a>
-                     // &nbsp;&nbsp; */}
-                      <a href="#">
-                        <i
-                          class="fa fa-instagram"
-                          aria-hidden="true"
-                          id="socialiconfix"
-                        ></i>
-                      </a>
-                      &nbsp;&nbsp;
-                    </center>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </center>
         </div>
       </div>
     </>
