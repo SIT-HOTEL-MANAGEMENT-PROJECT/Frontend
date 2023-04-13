19aa78da47e01987e7a3822ad950d7707caf530e
Prasun Roy <prasunroy212@gmail.com>
2023-04-13 23:38:18 +0530

    Added Props for Reservation Page

--- src/App.js
+++ src/App.js
@@ -192,7 +192,7 @@ const App = () => {
         <Route exact path="/Home" element={<Home />} />
         <Route exact path="/Home3" element={<Home3  resetAppData={resetAppData} />} />
         <Route exact path="/AllReservations" element={<AllReservations isAuthenticated={isAuthenticated} isUserAdmin={isUserAdmin} getLoggedInUserDetails={getLoggedInUserDetails} />} />
-        <Route exact path="/Reservation" element={<Reservation />} />
+        <Route exact path="/Reservation" element={<Reservation getLoggedInUserDetails={getLoggedInUserDetails}/>} />
         <Route
           exact
           path="/ReservationConfirmation"
