e5ea9c1dc972aff42aebde993de80af4e9ee5148
Prasun Roy <prasunroy212@gmail.com>
2023-04-15 23:40:20 +0530

    Integrated Backend in RoomBill Page

--- src/components/RoomBill.jsx
+++ src/components/RoomBill.jsx
@@ -1,6 +1,139 @@
 import React from 'react'
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Localbase from "localbase";
+import { useEffect } from "react";
+let db = new Localbase("hmctdb");
+db.config.debug = false;
+
 
 const RoomBill = () => {
+
+    const location = useLocation();
+
+    const [guestName, setGuestName] = useState({ title: "", firstname: "", middlename: "", lastname: "", });
+    const [travelAgentName, setTravelAgentName] = useState("");
+    const [guestPhoneNumber, setGuestPhoneNumber] = useState("");
+    const [companyName, setCompanyName] = useState("");
+    const [billNo, setBillNo] = useState("");
+    const [roomNumber, setRoomNumber] = useState("");
+    const [noOfRooms, setNoOfRooms] = useState("");
+    const [roomRate, setRoomRate] = useState("");
+    const [bookingDate, setBookingDate] = useState("");
+    const [reservationdepositeDate, setReservationdepositeDate] = useState("");
+    const [checkindepositeDate, setCheckindepositeDate] = useState("");
+    const [modeOfPayment, setModeOfPayment] = useState("");
+    const [roomType, setRoomType] = useState("");
+    const [discountAmount, setDiscountAmount] = useState("");
+    const [reservationDeposit, setReservationDeposit] = useState("");
+    const [checkinDeposit, setCheckinDeposit] = useState("");
+    const [arrivalDate, setArrivalDate] = useState("");
+    const [arrivalTime, setArrivalTime] = useState("");
+    const [departureDate, setdepartureDate] = useState("");
+    const [departureTime, setDepartureTime] = useState("");
+    const [registrationNo, setregistrationNo] = useState("");
+    const [gstId, setGstId] = useState("");
+    const [confirmationNo, setConfirmationNo] = useState("");
+    const [ttlCredit, setTtlCredit] = useState(0);
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            const query = new URLSearchParams(location.search);
+            const bookingid = query.get('bookingid');
+            if (bookingid && bookingid.length === 14) { getAndSetUserData(bookingid); }
+        }, 1000);
+    }, [location])
+
+
+    // Get :  Get all user data of a booking against bookingid
+    // params : bookingid
+    // return : 1. {success:true, data: {bookingid:"",name: {..}, phoneno: "", ...} }            IF ALL OK
+    //          2. {success: false, msg: 'Something Went Wrong'}                                 IF SERVER ERROR
+    //          3. {success:false, msg: "Invalid Booking Details"}                               IF BOOKING DATA NOT FOUND
+    const getUserDataAgainstBookingId = async (bookingid) => {
+        try {
+            let booking = await db.collection('reservation').doc({ bookingid: bookingid, checkedinstatus: "done" }).get();
+            if (!booking) { return { success: false, msg: "Invalid Booking Details" } }
+            return { success: true, data: booking };
+        } catch (e) {
+            console.log("CheckinPageError (getUserDataAgainstBookingId) : ", e);
+            return { success: false, msg: 'Something Went Wrong' }
+        }
+    }
+
+    const getAndSetUserData = async (bookingid) => {
+        let res = await getUserDataAgainstBookingId(bookingid);
+        if (res?.success) {
+            let booking = res?.data;
+            setGuestName(booking.name);
+            setTravelAgentName(booking.travelagentname);
+            setGuestPhoneNumber(booking.phoneno);
+            setCompanyName(booking.companyname);
+            setBillNo(booking.billingno);
+            setRoomNumber(booking.roomno);
+            setNoOfRooms(booking.noofrooms);
+            setRoomRate(booking.roomrate);
+            setArrivalDate(booking.arrivaldate);
+            setArrivalTime(booking.arrivaltime);
+            
+            //   setdepartureDate(booking.departuredate);
+            //   setDepartureTime(booking.departuretime);
+
+            setBookingDate(booking.bookingdate);
+            setModeOfPayment(booking.modeofpayment);
+            setDiscountAmount(booking.discountamount);
+            setRoomType(booking.typeofroom);
+            setregistrationNo(booking.bookingid);
+            setGstId(booking.gst);
+            setConfirmationNo(booking.confno);
+
+            let todayDate = new Date();
+            let todayDateString = todayDate.toISOString().slice(0, 10);
+            const hours = todayDate.getHours().toString().padStart(2, '0');
+            const minutes = todayDate.getMinutes().toString().padStart(2, '0');
+            const todayTimeString = `${hours}:${minutes}`;
+            setdepartureDate(todayDateString); setDepartureTime(todayTimeString);
+
+
+            let totalCredit = 0;
+
+            if(booking.discountamount && booking.discountamount != ""){
+                totalCredit += parseFloat(booking.discountamount);
+            }
+
+            for (const transaction of booking.paymenthistory) {
+                if(transaction.name == 'reservation'){
+                    setReservationdepositeDate(transaction.date);
+                    setReservationDeposit(transaction.credit);
+                    if(transaction.credit && transaction.credit != ""){
+                        totalCredit += parseFloat(transaction.credit);
+                    }
+                }
+                else if(transaction.name == 'checkin'){
+                    setCheckindepositeDate(transaction.date);
+                    setCheckinDeposit(transaction.credit);
+                    if(transaction.credit && transaction.credit != ""){
+                        totalCredit += parseFloat(transaction.credit);
+                    }
+                }
+            }
+
+            setTtlCredit(totalCredit);
+        } else {
+            setGuestName({ title: "", firstname: "", middlename: "", lastname: "", });
+            setTravelAgentName(''); setGuestPhoneNumber(''); setCompanyName(''); setBillNo('');
+            setRoomNumber(''); setNoOfRooms(''); setRoomRate(''); setArrivalDate('');
+            setArrivalTime(''); setdepartureDate(''); setDepartureTime('');
+
+            setBookingDate(''); setModeOfPayment(''); setDiscountAmount(''); setRoomType(''); setregistrationNo('');
+            setGstId(''); setConfirmationNo('');
+
+            setTtlCredit(0);
+        }
+    }
+
+
     return (
         <div>
             <div className="bg-light min-height-vh">
@@ -16,57 +149,57 @@ const RoomBill = () => {
                         <div className='col-md-6 medium-full-width'>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Guest Name :</h5>
-                                <h5 className='font-size-16 col-sm-8'>Prasun Roy</h5>
+                                <h5 className='font-size-16 col-sm-8'>{`${guestName.title} ${guestName.firstname} ${guestName.middlename} ${guestName.lastname}`}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Phone No :</h5>
-                                <h5 className='font-size-16 col-sm-8'>7432857244</h5>
+                                <h5 className='font-size-16 col-sm-8'>{guestPhoneNumber}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Travel Agent :</h5>
-                                <h5 className='font-size-16 col-sm-8'>Rajesh</h5>
+                                <h5 className='font-size-16 col-sm-8'>{travelAgentName}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Company :</h5>
-                                <h5 className='font-size-16 col-sm-8'>Roy Industries</h5>
+                                <h5 className='font-size-16 col-sm-8'>{companyName}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Bill No :</h5>
-                                <h5 className='font-size-16 col-sm-8'>134</h5>
+                                <h5 className='font-size-16 col-sm-8'>{billNo}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Payment :</h5>
-                                <h5 className='font-size-16 col-sm-8'>Credit Card</h5>
+                                <h5 className='font-size-16 col-sm-8'>{modeOfPayment}</h5>
                             </div>
                         </div>
                         <div className='col-md-6 medium-full-width'>
                             <div className='d-flex'>
-                                <h5 className='font-size-16 col-sm-4'>Room No :</h5>
-                                <h5 className='font-size-16 col-sm-8'>101,102</h5>
+                                <h5 className='font-size-16 col-sm-4'>Registration No :</h5>
+                                <h5 className='font-size-16 col-sm-8'>{registrationNo}</h5>
                             </div>
                             <div className='d-flex'>
-                                <h5 className='font-size-16 col-sm-4'>No of Room:</h5>
-                                <h5 className='font-size-16 col-sm-8'>2</h5>
+                                <h5 className='font-size-16 col-sm-4'>Confirmation No:</h5>
+                                <h5 className='font-size-16 col-sm-8'>{confirmationNo}</h5>
                             </div>
                             <div className='d-flex'>
-                                <h5 className='font-size-16 col-sm-4'>Room Rate :</h5>
-                                <h5 className='font-size-16 col-sm-8'>1000</h5>
+                                <h5 className='font-size-16 col-sm-4'>GST ID :</h5>
+                                <h5 className='font-size-16 col-sm-8'>{gstId}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Arrival Date :</h5>
-                                <h5 className='font-size-16 col-sm-3'>24/03/2023</h5>
+                                <h5 className='font-size-16 col-sm-3'>{arrivalDate}</h5>
                                 <h5 className='font-size-16 col-sm-1'>at</h5>
-                                <h5 className='font-size-16 col-sm-4'>08:00 A.M</h5>
+                                <h5 className='font-size-16 col-sm-4'>{arrivalTime}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Departure Date :</h5>
-                                <h5 className='font-size-16 col-sm-3'>14/04/2023</h5>
+                                <h5 className='font-size-16 col-sm-3'>{departureDate}</h5>
                                 <h5 className='font-size-16 col-sm-1'>at</h5>
-                                <h5 className='font-size-16 col-sm-4'>08:00 P.M</h5>
+                                <h5 className='font-size-16 col-sm-4'>{departureTime}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Original Bill Date :</h5>
-                                <h5 className='font-size-16 col-sm-8'>14/04/2023</h5>
+                                <h5 className='font-size-16 col-sm-8'>{departureDate}</h5>
                             </div>
                         </div>
                     </div>
@@ -84,52 +217,44 @@ const RoomBill = () => {
                             </thead>
                             <tbody className="table-group-divider">
                                 <tr className='hover-gray make-cursor-pointer' >
-                                    <td>Date</td>
-                                    <td>1</td>
-                                    <td>102</td>
-                                    <td>Standard</td>
-                                    <td>Debit</td>
-                                    <td>Credit</td>
-                                </tr>
-                                <tr className='hover-gray make-cursor-pointer' >
-                                    <td>Date</td>
-                                    <td>2</td>
-                                    <td>201,202</td>
-                                    <td>Delux</td>
-                                    <td>Debit</td>
-                                    <td>Credit</td>
+                                    <td>{bookingDate}</td>
+                                    <td>{noOfRooms}</td>
+                                    <td>{roomNumber}</td>
+                                    <td>{roomType}</td>
+                                    <td>{roomRate}</td>
+                                    <td>{""}</td>
                                 </tr>
                                 <tr>
-                                    <td></td>
+                                    <td>{bookingDate}</td>
                                     <td></td>
                                     <td></td>
                                     <td className="text-dark">Discount</td>
-                                    <td>Debit</td>
-                                    <td className="text-dark">Credit</td>
+                                    <td>{""}</td>
+                                    <td className="text-dark">{discountAmount}</td>
                                 </tr>
-                                <tr>
-                                    <td></td>
+                                {reservationDeposit && <tr>
+                                    <td>{reservationdepositeDate}</td>
                                     <td></td>
                                     <td></td>
                                     <td className="text-dark">Reservation Deposit</td>
-                                    <td>Debit</td>
-                                    <td className="text-dark">Credit</td>
-                                </tr>
-                                <tr>
-                                    <td></td>
+                                    <td>{""}</td>
+                                    <td className="text-dark">{reservationDeposit}</td>
+                                </tr>}
+                                {checkinDeposit && <tr>
+                                    <td>{checkindepositeDate}</td>
                                     <td></td>
                                     <td></td>
                                     <td className="text-dark">CheckIn Deposit</td>
-                                    <td>Debit</td>
-                                    <td className="text-dark">Credit</td>
-                                </tr>
+                                    <td>{""}</td>
+                                    <td className="text-dark">{checkinDeposit}</td>
+                                </tr>}
                                 <tr>
                                     <td></td>
                                     <td></td>
                                     <td></td>
                                     <td className="text-dark">Total Amount</td>
-                                    <td>Debit</td>
-                                    <td>Credit</td>
+                                    <td>{roomRate}</td>
+                                    <td>{ttlCredit}</td>
                                 </tr>
                             </tbody>
                         </table>
