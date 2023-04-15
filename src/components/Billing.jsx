1ae0273860507b1c91d836aeefbd8ee728ab208c
Prasun Roy <prasunroy212@gmail.com>
2023-04-15 16:42:03 +0530

    Integrated Backend in Billing Page

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,6 +1,110 @@
 import React from 'react'
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Localbase from "localbase";
+import { useEffect } from "react";
+let db = new Localbase("hmctdb");
+db.config.debug = false;
+
 
 const Billing = () => {
+    const location = useLocation();
+    
+    const [guestName, setGuestName] = useState({title: "", firstname: "", middlename: "", lastname: "", });
+    const [travelAgentName, setTravelAgentName] = useState("");
+    const [guestPhoneNumber, setGuestPhoneNumber] = useState("");
+    const [companyName, setCompanyName] = useState("");
+    const [gstId, setGstId] = useState("");
+    const [billing, setBilling] = useState("");
+    const [billNo, setBillNo] = useState("");
+    const [confirmationNo, setConfirmationNo] = useState("");
+    const [roomNumber, setRoomNumber] = useState("");
+    const [noOfRooms, setNoOfRooms] = useState("");
+    const [roomRate, setRoomRate] = useState("");
+    const [guestsNo, setGuestsNo] = useState("");
+    const [arrivalDate, setArrivalDate] = useState("");
+    const [arrivalTime, setArrivalTime] = useState("");
+    const [departureDate, setdepartureDate] = useState("");
+    const [departureTime, setDepartureTime] = useState("");
+    const [registrationNo, setregistrationNo] = useState("");
+    const [paymentData, setPaymentData] = useState([]);
+    const [ttlDebit, setTtlDebit] = useState(0);
+    const [ttlCredit, setTtlCredit] = useState(0);
+
+      
+    useEffect(() => {
+      setTimeout(() => {
+        const query = new URLSearchParams(location.search);
+        const bookingid = query.get('bookingid');
+        if (bookingid && bookingid.length === 14) { getAndSetUserData(bookingid); }
+      }, 1000);
+    }, [location])
+  
+ 
+    // Get :  Get all user data of a booking against bookingid
+    // params : bookingid
+    // return : 1. {success:true, data: {bookingid:"",name: {..}, phoneno: "", ...} }            IF ALL OK
+    //          2. {success: false, msg: 'Something Went Wrong'}                                 IF SERVER ERROR
+    //          3. {success:false, msg: "Invalid Booking Details"}                               IF BOOKING DATA NOT FOUND
+    const getUserDataAgainstBookingId = async (bookingid) => {
+        try {
+            let booking = await db.collection('reservation').doc({ bookingid: bookingid, checkedinstatus: "done", checkedoutstatus: "pending" }).get();
+            if (!booking) { return { success: false, msg: "Invalid Booking Details" } }
+            return { success: true, data: booking };
+        } catch (e) {
+            console.log("CheckinPageError (getUserDataAgainstBookingId) : ", e);
+            return { success: false, msg: 'Something Went Wrong' }
+        }
+    }
+
+    const getAndSetUserData = async (bookingid) => {
+        let res = await getUserDataAgainstBookingId(bookingid);
+        if (res?.success) {
+          let booking = res?.data;
+          setGuestName(booking.name);
+          setTravelAgentName(booking.travelagentname);
+          setGuestPhoneNumber(booking.phoneno);
+          setCompanyName(booking.companyname);
+          setGstId(booking.gst);
+          setBilling(booking.billing);
+          setBillNo(booking.billingno);
+          setConfirmationNo(booking.confno);
+          setRoomNumber(booking.roomno);
+          setNoOfRooms(booking.noofrooms);
+          setRoomRate(booking.roomrate);
+          setGuestsNo(booking.phoneno);
+          setArrivalDate(booking.arrivaldate);
+          setArrivalTime(booking.arrivaltime);
+          setdepartureDate(booking.departuredate);
+          setDepartureTime(booking.departuretime);
+          setPaymentData(booking.paymenthistory);
+          setregistrationNo(booking.bookingid);
+
+          let totalDebit = 0;
+          let totalCredit = 0;
+    
+          for (const transaction of booking.paymenthistory) {
+            if (transaction.debit !== '') {
+              totalDebit += parseFloat(transaction.debit);
+            }
+            if (transaction.credit !== '') {
+              totalCredit += parseFloat(transaction.credit);
+            }
+          }
+    
+          setTtlDebit(totalDebit);
+          setTtlCredit(totalCredit);
+        } else {
+          setGuestName({ title: "", firstname: "", middlename: "", lastname: "", });
+          setTravelAgentName(''); setGuestPhoneNumber(''); setCompanyName(''); setGstId(''); setBilling(''); setBillNo('');
+          setConfirmationNo(''); setRoomNumber(''); setNoOfRooms(''); setRoomRate(''); setGuestsNo(''); setArrivalDate('');
+          setArrivalTime(''); setdepartureDate(''); setDepartureTime(''); setPaymentData([]); setregistrationNo('');
+    
+          setTtlDebit(0);
+          setTtlCredit(0);
+        }
+    }
+
     return (
         <div>
             <div className="bg-light min-height-vh">
@@ -16,73 +120,73 @@ const Billing = () => {
                         <div className='col-md-6 medium-full-width'>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Guest Name :</h5>
-                                <h5 className='font-size-16 col-sm-8'>Prasun Roy</h5>
+                                <h5 className='font-size-16 col-sm-8'>{`${guestName.title} ${guestName.firstname} ${guestName.middlename} ${guestName.lastname}`}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Phone No :</h5>
-                                <h5 className='font-size-16 col-sm-8'>7432857244</h5>
+                                <h5 className='font-size-16 col-sm-8'>{guestPhoneNumber}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Travel Agent :</h5>
-                                <h5 className='font-size-16 col-sm-8'>Rajesh</h5>
+                                <h5 className='font-size-16 col-sm-8'>{travelAgentName}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Company :</h5>
-                                <h5 className='font-size-16 col-sm-8'>Roy Industries</h5>
+                                <h5 className='font-size-16 col-sm-8'>{companyName}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>GST :</h5>
-                                <h5 className='font-size-16 col-sm-8'>Asdpp56879</h5>
+                                <h5 className='font-size-16 col-sm-8'>{gstId}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Billing :</h5>
-                                <h5 className='font-size-16 col-sm-8'>ghujki89</h5>
+                                <h5 className='font-size-16 col-sm-8'>{billing}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Bill No :</h5>
-                                <h5 className='font-size-16 col-sm-8'>134</h5>
+                                <h5 className='font-size-16 col-sm-8'>{billNo}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Confirmation No :</h5>
-                                <h5 className='font-size-16 col-sm-8'>1235</h5>
+                                <h5 className='font-size-16 col-sm-8'>{confirmationNo}</h5>
                             </div>
                         </div>
                         <div className='col-md-6 medium-full-width'>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Room No :</h5>
-                                <h5 className='font-size-16 col-sm-8'>101,102</h5>
+                                <h5 className='font-size-16 col-sm-8'>{roomNumber}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>No of Room:</h5>
-                                <h5 className='font-size-16 col-sm-8'>2</h5>
+                                <h5 className='font-size-16 col-sm-8'>{noOfRooms}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Room Rate :</h5>
-                                <h5 className='font-size-16 col-sm-8'>1000</h5>
+                                <h5 className='font-size-16 col-sm-8'>{roomRate}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Guests No :</h5>
-                                <h5 className='font-size-16 col-sm-8'>101</h5>
+                                <h5 className='font-size-16 col-sm-8'>{guestPhoneNumber}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Arrival Date :</h5>
-                                <h5 className='font-size-16 col-sm-3'>24/03/2023</h5>
+                                <h5 className='font-size-16 col-sm-3'>{arrivalDate}</h5>
                                 <h5 className='font-size-16 col-sm-1'>at</h5>
-                                <h5 className='font-size-16 col-sm-4'>08:00 A.M</h5>
+                                <h5 className='font-size-16 col-sm-4'>{arrivalTime}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Departure Date :</h5>
-                                <h5 className='font-size-16 col-sm-3'>14/04/2023</h5>
+                                <h5 className='font-size-16 col-sm-3'>{departureDate}</h5>
                                 <h5 className='font-size-16 col-sm-1'>at</h5>
-                                <h5 className='font-size-16 col-sm-4'>08:00 P.M</h5>
+                                <h5 className='font-size-16 col-sm-4'>{departureTime}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Registration No :</h5>
-                                <h5 className='font-size-16 col-sm-8'>1015102</h5>
+                                <h5 className='font-size-16 col-sm-8'>{registrationNo}</h5>
                             </div>
                             <div className='d-flex'>
                                 <h5 className='font-size-16 col-sm-4'>Original Bill Date :</h5>
-                                <h5 className='font-size-16 col-sm-8'>14/04/2023</h5>
+                                <h5 className='font-size-16 col-sm-8'>{departureDate}</h5>
                             </div>
                         </div>
                     </div>
@@ -98,33 +202,21 @@ const Billing = () => {
                                 </tr>
                             </thead>
                             <tbody className="table-group-divider">
-                                <tr className='hover-gray make-cursor-pointer' >
-                                    <td>Date</td>
-                                    <td>Accomodation</td>
-                                    <td>Room</td>
-                                    <td>Debit</td>
-                                    <td>Credit</td>
-                                </tr>
-                                <tr className='hover-gray make-cursor-pointer' >
-                                    <td>Date</td>
-                                    <td>Accomodation SGST @9% </td>
-                                    <td>Room</td>
-                                    <td>Amount</td>
-                                    <td>Amount</td>
-                                </tr>
-                                <tr className='hover-gray make-cursor-pointer' >
-                                    <td>Date</td>
-                                    <td>Accomodation CGST @9% </td>
-                                    <td>Room</td>
-                                    <td>Amount</td>
-                                    <td>Amount</td>
-                                </tr>
+                                {paymentData && paymentData.map((item, index) => {
+                                    return <tr key={index + 1} className='hover-gray make-cursor-pointer' >
+                                    <td>{item.date}</td>
+                                    <td>{item.description}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.debit}</td>
+                                    <td>{item.credit}</td>
+                                  </tr>
+                                })}
                                 <tr className='hover-gray make-cursor-pointer table-br-last-blue' >
                                     <td className="table-tdata"></td>
                                     <td className="table-tdata"></td>
                                     <td className="table-tdata"></td>
-                                    <td className="table-tdata">Debit</td>
-                                    <td className="table-tdata">Credit</td>
+                                    <td className="table-tdata">{ttlDebit}</td>
+                                    <td className="table-tdata">{ttlCredit}</td>
                                 </tr>
                             </tbody>
                         </table>
